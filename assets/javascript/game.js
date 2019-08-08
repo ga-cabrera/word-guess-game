994c9d050a11ab41962775a2ed0c36c27cf8d3a3
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-08-07 22:07:13 -0700

    added list of words and word randomizer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,3 +6,17 @@
 // Step 5: Determine whether letter/guess is true or false
 //  * If true, empty space will be filled with correct letter
 //  * If false, incorrect letter will be added to "wrong letter" pile
+var wordList = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten"
+];
+
+var word = wordlist[Math.floor(Math.random() * wordlist.length)];
