dc2b4e4511c78c0fb50eef01f3f10cd75c7e7e59
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-08-07 21:38:08 -0700

    added initial code to html page, wrote out my pseudo code in js page

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+// My Pseudo Code :
+// Step 1: List of words
+// Step 2: Pick a word
+// Step 3: Create spaces for letters of words (underscores)
+// Step 4: Player picks letter/ guesses word
+// Step 5: Determine whether letter/guess is true or false
+//  * If true, empty space will be filled with correct letter
+//  * If false, incorrect letter will be added to "wrong letter" pile
